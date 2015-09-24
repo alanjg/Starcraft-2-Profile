ac660c598e234a3ea2e3c6c51df4ff32fe82aa41
alanjg <alanga@ALANGAMACWIN10.redmond.corp.microsoft.com>
2015-09-24 15:26:24 -0700

    Initial checkin of SC2 battle net profile viewer
    Cordova app that lets you log in with battle.net credentials and shows
    the user's profile.

---
+++ www/scripts/battleNetLoader.js
@@ -0,0 +1,102 @@
+﻿function loadResourceAsync(url) {
+	return new WinJS.Promise(function (comp, err, prog) {
+		var req = new XMLHttpRequest();
+		function handler() {
+			if (req.readyState == 4) {
+				if (req.status == 200) {
+					var profileObj = JSON.parse(req.responseText);
+					comp(profileObj);
+				} else {
+					err(req)
+				}
+			}
+		}
+
+		req.open("GET", url, true);
+		req.onreadystatechange = handler;
+		req.send();
+		prog();
+	});
+}
+
+function processProfile(profile, achievements, rewards, achievementsJson, rewardsJson) {
+	var portrait = document.getElementById('profilePicture');
+	portrait.style.width = profile.portrait.w + "px";
+	portrait.style.height = profile.portrait.h + "px";
+	portrait.style.backgroundImage = "url(" + profile.portrait.url + ")";
+	portrait.style.backgroundPositionX = profile.portrait.x + "px";
+	portrait.style.backgroundPositionY = profile.portrait.y + "px";
+
+	var portraitBorder = document.getElementById('profileBorder');
+	var league = profile.career.league;
+	portraitBorder.style.backgroundImage = "url('images/portrait-summary-" + league + ".png')";
+
+	var name = document.getElementById('displayName');
+	name.textContent = profile.displayName;
+	for (var i = 0; i < profile.rewards.earned.length; i++) {
+		var id = profile.rewards.earned[i];
+		for (var j = 0; j < rewardsJson.portraits.length; j++) {
+			var a = rewardsJson.portraits[j];
+			if (a.id == id) {
+				rewards.push(
+					{
+						url: "url(" + a.icon.url + ")",
+						w: a.icon.w + "px",
+						h: a.icon.h + "px",
+						x: a.icon.x + "px",
+						y: a.icon.y + "px",
+						title: a.title
+					});
+			}
+		}
+	}
+
+	for (i = 0; i < profile.achievements.achievements.length; i++) {
+		var id = profile.achievements.achievements[i].achievementId;
+		for (j = 0; j < achievementsJson.achievements.length; j++) {
+			var a = achievementsJson.achievements[j];
+			if (a.achievementId == id) {
+				achievements.push(
+					{
+						url: "url(" + a.icon.url + ")",
+						w: a.icon.w + "px",
+						h: a.icon.h + "px",
+						x: a.icon.x + "px",
+						y: a.icon.y + "px",
+						title: a.title,
+						description: a.description
+					});
+			}
+		}
+	}
+}
+
+function processMatches(matches, games) {
+	for (i = 0; i < matches.matches.length; i++) {
+		games.push(matches.matches[i]);
+	}
+}
+
+function loadProfile(profilePath, data) {
+	var games = data.games;
+	var achievements = data.achievements;
+	var rewards = data.rewards;
+
+	var baseUrl = "http://us.battle.net/api/sc2";
+	var profileUrl = baseUrl + profilePath;
+	var laddersUrl = profileUrl + "ladders";
+	var matchesUrl = profileUrl + "matches";
+	var achievementsUrl = baseUrl + "/data/achievements";
+	var rewardsUrl = baseUrl + "/data/rewards";
+
+	loadResourceAsync(profileUrl).then(function (profile) {
+		loadResourceAsync(achievementsUrl).then(function (achievementsJson) {
+			loadResourceAsync(rewardsUrl).then(function (rewardsJson) {
+				processProfile(profile, achievements, rewards, achievementsJson, rewardsJson);
+			});
+		});
+	});
+	loadResourceAsync(matchesUrl).then(function (matches) {
+		processMatches(matches, games);
+	});
+}
---
+++ www/scripts/index.js
@@ -0,0 +1,84 @@
+﻿(function () {
+    "use strict";
+
+    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+
+    function onDeviceReady() {
+        // Handle the Cordova pause and resume events
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener('resume', onResume.bind(this), false);
+        var games = new WinJS.Binding.List();
+        var achievements = new WinJS.Binding.List();
+        var rewards = new WinJS.Binding.List();
+        var data = {
+        	games: games,
+        	achievements: achievements,
+        	rewards: rewards
+        };
+        WinJS.Namespace.define("Data", data);
+        WinJS.Application.Data = data;
+        WinJS.UI.processAll();
+        document.getElementById('loginClick').addEventListener('click', onLoginClick.bind(this), false);
+        
+    };
+
+    function onPause() {
+    };
+
+    function onResume() {
+    };
+
+    function postTokenRequest(url, params, user, password) {
+    	return new WinJS.Promise(function (comp, err, prog) {
+    		var req = new XMLHttpRequest();
+    		function handler() {
+    			if (req.readyState == 4) {
+    				if (req.status == 200) {
+    					var profileObj = JSON.parse(req.responseText);
+    					comp(profileObj);
+    				} else {
+    					err(req)
+    				}
+    			}
+    		}
+
+    		req.open("POST", url, true);
+    		req.onreadystatechange = handler;	
+    		req.setRequestHeader('Authorization', 'Basic ' + window.btoa(unescape(encodeURIComponent(user + ':' + password))));
+    		req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    		req.setRequestHeader("Content-length", params.length);
+    		req.setRequestHeader("Connection", "close");
+    		req.send(params);
+    		prog();
+    	});
+    }
+    function onLoginClick() {
+    	var scope = 'sc2.profile';
+
+    	var ref = window.open('https://us.battle.net/oauth/authorize?client_id=' + WinJS.Application.clientId + '&scope=' + scope + '&redirect_uri=https://localhost/callback&response_type=code', '_blank', 'location=no');
+    	ref.addEventListener('loadstart', function (event) {
+    		if ((event.url).indexOf("https://localhost/callback") == 0) {
+    			var requestToken = (event.url).split("code=")[1];
+				
+    			ref.close();
+				/*
+    			ref = window.open('http://us.battle.net/sc2/en/profile/friends', '_blank', 'location=no');
+    			ref.addEventListener('loadstart', function (event) {
+    				if ((event.url).indexOf("https://localhost/callback") == 0) {
+
+    				}
+    			});
+				*/
+    			postTokenRequest('https://us.battle.net/oauth/token', 'grant_type=authorization_code&code=' + requestToken + '&redirect_uri=https://localhost/callback&scope=' + scope, WinJS.Application.clientId, WinJS.Application.secret).then(function (results) {
+    				var token = results.access_token;
+    				var profileUrl = 'https://us.api.battle.net/sc2/profile/user?locale=en_US&access_token=' + token;
+    				loadResourceAsync(profileUrl).then(function (profile) {
+    					var profilePath = profile.characters[0].profilePath;
+    					
+    					loadProfile(profilePath, WinJS.Application.Data);
+    				});
+    			});
+    		}
+    	});
+    };
+} )();
